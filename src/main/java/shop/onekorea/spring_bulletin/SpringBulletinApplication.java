93b9d47dffb5337c36191cdac75e757833fcf006
rwkang <rwkang@naver.com>
2023-08-02 17:10:27 +0900

    added application.yml created project

---
+++ src/main/java/shop/onekorea/spring_bulletin/SpringBulletinApplication.java
@@ -0,0 +1,16 @@
+package shop.onekorea.spring_bulletin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBulletinApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBulletinApplication.class, args);
+		System.out.println("================================> Project Spring Bulletin Application is running by Ro Won Kang");
+
+	}
+
+}
---
+++ src/test/java/shop/onekorea/spring_bulletin/SpringBulletinApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.onekorea.spring_bulletin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBulletinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
